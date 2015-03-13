10f782bbcbca4e622b84ff044f8b9c0731cc7a2d
alexp <alexp@ncc.snt.ru>
2015-03-13 12:25:20 +0600

    Lab4

---
+++ Lab4/src/ru/pere/lab4/Main.java
@@ -0,0 +1,22 @@
+package ru.pere.lab4;
+
+import static java.lang.System.out;
+
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		out.println("Ведите имя: ");
+		//String name = reader.readLine();
+		//String helo = String.format("Привет, %s", name);
+		//out.println(helo);
+		//out.printf("Привет, %s", name);
+		out.printf("Привет, %s", reader.readLine());
+
+
+	}
+
+}
