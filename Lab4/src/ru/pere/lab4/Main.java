ecdd84a1307110330792be6913d30b5c8be8e3e1
alexp <alexp@ncc.snt.ru>
2015-03-13 14:44:00 +0600

    Lab4 v2

--- Lab4/src/ru/pere/lab4/Main.java
+++ Lab4/src/ru/pere/lab4/Main.java
@@ -9,12 +9,17 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
-		out.println("Ведите имя: ");
+		out.print("Ведите имя: ");
 		//String name = reader.readLine();
 		//String helo = String.format("Привет, %s", name);
 		//out.println(helo);
 		//out.printf("Привет, %s", name);
-		out.printf("Привет, %s", reader.readLine());
+		//out.printf("Привет, %s", reader.readLine());
+		StringBuffer sb = new StringBuffer();
+		sb.append("Привет, ");
+		sb.append(reader.readLine());
+		sb.append("!");
+		out.println(sb.toString());
 
 
 	}
